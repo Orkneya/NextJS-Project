497aa4a50fd3c18aa2262ed854f56ebb8f7c5ecd
Tetyana Popenko <orkneya17@gmail.com>
2025-12-05 22:11:48 +0200

    dashboard finish

--- app/(admin)/companies/[id]/page.tsx
+++ app/(admin)/companies/[id]/page.tsx
@@ -1,14 +1,38 @@
-import React, { Children } from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import Header from "../../../components/header";
+import { notFound } from "next/navigation";
 
 export interface PageProps {
   params: { id: string };
 }
 
 export default function Page({ params }: PageProps) {
+  useEffect(() => {
+    const id = Number.parseInt(params.id);
+    if (Number.isNaN(id)) {
+      notFound();
+    }
+  }, [params.id]);
   return (
     <>
       <Header>Company ({params.id})</Header>
     </>
   );
 }
+
+// import React, { Children } from "react";
+// import Header from "../../../components/header";
+
+// export interface PageProps {
+//   params: { id: string };
+// }
+
+// export default function Page({ params }: PageProps) {
+//   return (
+//     <>
+//       <Header>Company ({params.id})</Header>
+//     </>
+//   );
+// }
