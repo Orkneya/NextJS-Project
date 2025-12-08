68c18521b90b0e70cebe08fd81a766afb5ebc20d
Tetyana Popenko <orkneya17@gmail.com>
2025-12-09 01:13:08 +0200

    company item

--- app/(admin)/companies/@header/[id]/page.tsx
+++ app/(admin)/companies/@header/[id]/page.tsx
@@ -1,34 +1,22 @@
 import React from "react";
-import Header from "../../../../components/header";
+import { Company, getCompany } from "@/lib/api";
+import getQueryClient from "@/lib/utils/getQueryClient";
+import Header from "@/app/components/header";
 
 export interface PageProps {
   params: { id: string };
 }
 
-export default function Page({ params }: PageProps) {
-  return <Header>{`Company (${params.id})`}</Header>;
+export default async function Page({ params }: PageProps) {
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["companies", params.id],
+    queryFn: () => getCompany(params.id, { cache: "no-store" }),
+    staleTime: 10 * 1000,
+  });
+
+  const company = queryClient.getQueryData(["companies", params.id]) as Company;
+
+  return <Header>{company?.title}</Header>;
 }
-
-// "use client";
-
-// import React, { useEffect } from "react";
-// import { notFound } from "next/navigation";
-// import Header from "../../../../components/header";
-
-// export interface PageProps {
-//   params: { id: string };
-// }
-
-// export default function Page({ params }: PageProps) {
-//   useEffect(() => {
-//     const id = Number.parseInt(params.id);
-//     if (Number.isNaN(id)) {
-//       notFound();
-//     }
-//   }, [params.id]);
-//   return (
-//     <>
-//       <Header>Company ({params.id})</Header>
-//     </>
-//   );
-// }
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,7 @@
-import AddCompanyButton from "./components/add-company-button";
-// import MagicButton from "./components/magic-button";
-import StatusLabel, { Status } from "./components/status-label";
-import { headers } from "next/headers";
-
 export default function Home() {
-  const allHeaders = headers();
-  console.log("Server headers:", Object.fromEntries(allHeaders.entries()));
   return (
     <main>
-      <h1 className="text-xl"> Page Home </h1>
-      {/* <h1 className="text-xl"> Page Home {new Date().toTimeString()} </h1> */}
-      <div className="flex gap-2 mb-4">
-        <StatusLabel status={Status.Active}>Active</StatusLabel>
-        <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-        <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-        <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
-      </div>
-      <AddCompanyButton />
-      {/* <MagicButton /> */}
+      <h1 className="text-xl">Home page</h1>
     </main>
   );
 }
